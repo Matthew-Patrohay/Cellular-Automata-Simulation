c282c8406eb1f7bd1102fdae9f2e4f0e8853abf2
Matthew Patrohay <matthewpatrohay@gmail.com>
2022-01-07 23:01:33 -0500

    Initial Commit

---
+++ Basic OpenGL C Template/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Basic OpenGL C Template
+//
+//  Created by Matthew Patrohay on 1/7/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
