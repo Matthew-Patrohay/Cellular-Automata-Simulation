36864154b4a4cf49a4f6b7db6e76973f24afe4e3
Matthew-Patrohay <matthewpatrohay@gmail.com>
2022-01-07 23:21:27 -0500

    Hello World Example Code

--- Basic OpenGL C Template/main.c
+++ Basic OpenGL C Template/main.c
@@ -1,14 +1,37 @@
-//
-//  main.c
-//  Basic OpenGL C Template
-//
-//  Created by Matthew Patrohay on 1/7/22.
-//
+#include <GLFW/glfw3.h>
 
-#include <stdio.h>
+int main(void)
+{
+    GLFWwindow* window;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World in GitHub!\n");
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
